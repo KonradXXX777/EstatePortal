b172d3cb93918470a0f4ada3beb6e09456b11d6e
KonradXXX777 <konrad.sieradzki@onet.pl>
2024-10-10 12:28:25 +0200

    delete all files

--- EstatePortal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EstatePortal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
