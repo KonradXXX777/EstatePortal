7952d5459eeb1f1d6c2e32459d4de500a1503e9e
KonradXXX777 <konrad.sieradzki@onet.pl>
2024-10-10 12:21:30 +0200

    delete branch

--- EstatePortal/obj/Debug/net8.0/EstatePortal.AssemblyInfo.cs
+++ EstatePortal/obj/Debug/net8.0/EstatePortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstatePortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1466c455ca061323578416eae18aa22da4b654b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstatePortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstatePortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
